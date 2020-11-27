176fcb8a33fe5a6213c980fc1abbbfb9cd9562d0
Kale Kundert <kale@thekunderts.net>
2020-11-27 13:05:47 -0500

    Add tests for issue #16.

---
+++ tests/issues/dummy_repo_16/run_vim_16.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+
+ln -sf git .git
+HOME=../dummy_home_16 vim test_16.py
---
+++ tests/issues/dummy_repo_16/test_16.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# The bar() function should have gitgutter signs and the number of lines in 
+# each fold should be aligned with the right side of the window.
+
+def foo():
+    pass
+
+def bar():
+    pass
